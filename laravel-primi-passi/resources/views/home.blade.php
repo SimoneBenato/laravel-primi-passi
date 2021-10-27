27b1e853bd2d66fa403d3ed0b975baede17c9405
SimoneBenato <sb270298@gmail.com>
2021-10-27 09:16:10 +0200

    Esercizio completato

---
+++ laravel-primi-passi/resources/views/home.blade.php
@@ -0,0 +1,27 @@
+<!DOCTYPE html>
+<html lang="{{ str_replace('_', '-', app()->getLocale()) }}">
+    <head>
+        <meta charset="utf-8">
+        <meta name="viewport" content="width=device-width, initial-scale=1">
+
+        <title>Hello World</title>
+
+        <!-- Styles -->
+        <style>
+            
+        </style>
+    </head>
+    <body>
+        <div class="content">
+            <h1>
+                Hello World
+            </h1>
+
+            <div class="links">
+                <a href="/home">Home</a>
+                <a href="/work">Work</a>
+                <a href="/sport">Sport</a>
+            </div>
+        </div>
+    </body>
+</html>
